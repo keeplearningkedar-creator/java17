9f41884ce35db7edaec63eb07faf17526d92d936
Kedar <upskillkedar@gmail.com>
2025-11-09 12:33:43 -0600

    Test commit

---
+++ src/codesignal/TwoNumberSum.java
@@ -0,0 +1,35 @@
+package codesignal;
+
+import java.util.*;
+
+class TwoNumberSum {
+    public static void main(String[] args) {
+        int[] inputArray = {3, 5, -4, 8, 11, 1, -1, 6};
+        int []result = twoNumberSum(inputArray, 10);
+        System.out.println("Printing Two Numbers Having Given Sum "+result[0] + ","+ result[1]);
+    }
+    public static int[] twoNumberSum(int[] array, int targetSum) {
+
+        if(array.length == 0) return new int[0];
+
+        HashMap<Integer, Integer> numberIndexMap = new HashMap<>();
+        int[] resultArray = new int[2];
+        boolean foundMatchingNumbers = false;
+        for(int i =0; i < array.length; i++) {
+            int complement = targetSum - array[i];
+            if(numberIndexMap.containsKey(complement)) {
+                resultArray[0] = array[i];
+                int numberIndex = numberIndexMap.get(complement);
+                resultArray[1] = array[numberIndex];
+                foundMatchingNumbers = true;
+                break;
+            } else {
+                numberIndexMap.put(array[i], i);
+            }
+        }
+        if(!foundMatchingNumbers) return new int[0];
+
+        return resultArray;
+    }
+}
+
